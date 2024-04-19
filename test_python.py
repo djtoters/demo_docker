e1fe904c85adfd9b4afe33214753967c86720bb8
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-19 09:21:14 +0200

    commit black OK

--- test_python.py
+++ test_python.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class PartnerMixin(models.AbstractModel):
     """
     Nouvelle classe abstraite qui va être utilisée comme mixin pour afficher
-    les champs related des partenaires
+    les champs related des partenaires ttest
     """
 
     _name = "partner.mixin"
